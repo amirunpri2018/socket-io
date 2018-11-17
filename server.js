a37e4a012cbe057031e0b5dc1d1eba772645b010
varid68 <varid68@gmil.com>
2018-11-18 02:03:31 +0700

    deploy node js

---
+++ server.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function (socket) {
+  socket.on('newMessage', function (msg) {
+    io.emit('newMessage', msg);
+    console.log(msg);
+  });
+
+  socket.on('disconnect', function (msg) {
+    console.log('user disconnect');
+  });
+});
+
+
+http.listen(3000, function () {
+  console.log('listening on 3000');
+})
